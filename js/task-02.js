9a3e295bfa2713a16638c3ecdba0ad177a827a09
Dmytro Savitskyi <zefir0211@gmail.com>
2023-05-11 20:42:26 +0200

    homeWork2/10

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// const ingredientsEl = document.querySelector("#ingredients");
+// console.log("ingredientsEl:", ingredientsEl)
+
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const element = ingredients.map(ingredient => {
+  const newIngredientsList = document.createElement("li");
+  newIngredientsList.textContent = ingredient;
+
+  return newIngredientsList;
+})
+
+ingredientsEl.append(...element)
