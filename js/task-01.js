d82a030d92fa5742669415077cbeaf3bffd3195a
Dmytro Savitskyi <zefir0211@gmail.com>
2023-05-11 19:59:17 +0200

    HomeWork1/10

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,13 @@
+const numberOfCategories = document.querySelector("#categories");
 
 
+const categoryAnimals = numberOfCategories.querySelectorAll(".item");
+console.log(`Number of categories: ${categoryAnimals.length}`)
 
-
+categoryAnimals.forEach(item => {
+    const categoryName = item.querySelector('h2').textContent;
+    const categoryElements = item.querySelectorAll('li').length;
+    
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${categoryElements}\n`);
+});
