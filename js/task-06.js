644c58b36b62992c9da8e327523ba34e52ac0f48
Dmytro Savitskyi <zefir0211@gmail.com>
2023-05-18 21:35:47 +0200

    homeWork-6/10

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const inputEl = document.querySelector('#validation-input')
+const lengthEL = inputEl.getAttribute('data-length')
+
+const handleInputColor = (e) => {
+if (e.target.value.length < lengthEL) {
+    e.target.classList.add('invalid')
+} else {
+    e.target.classList.add('valid')
+    e.target.classList.remove('invalid')
+}
+}
+
+inputEl.addEventListener('blur', handleInputColor)
+
+
